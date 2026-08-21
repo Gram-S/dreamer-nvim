585622a1452e34cc53c8f1f8b8a4a35ebcf4f5e4
Gram-S <grantms4455@gmail.com>
2026-08-20 20:45:18 -0600

    A little smart directory movement in oil

--- lua/dorothys-nvim/plugin-mappings.lua
+++ lua/dorothys-nvim/plugin-mappings.lua
@@ -13,3 +13,32 @@ end)
 
 -- Telescope to open the file history
 vim.keymap.set('n', 'H', '<cmd>Telescope oldfiles<CR>')
+
+-- Only change cwd when opening a file AFTER being in Oil
+local was_in_oil = false
+
+vim.api.nvim_create_autocmd({"FileType", "BufReadPost"}, {
+  group = vim.api.nvim_create_augroup("OilCwdSync", {}),
+  pattern = "*",
+  callback = function()
+    -- Detect leaving Oil buffer
+    if vim.bo.filetype == "oil" then
+      was_in_oil = true
+      return
+    end
+    
+    -- Only update if coming from Oil + this is a real file
+    if was_in_oil and vim.bo.buftype == "" then
+      local dir = vim.fn.expand("%:p:h")
+      if dir and vim.fn.isdirectory(dir) == 1 then
+        vim.cmd("cd " .. vim.fn.fnameescape(dir))
+        was_in_oil = false
+      end
+    end
+    
+    -- Reset flag when entering non-Oil buffer
+    if vim.bo.filetype ~= "oil" then
+      was_in_oil = false
+    end
+  end,
+})
